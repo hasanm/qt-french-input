e0c8aaf3c218aaa877728608ba94bf04eeaa3dc0
Mahmudul Hasan <hasan@uleth.ca>
2024-02-28 09:55:01 -0700

    Modified Files

--- main.cpp
+++ main.cpp
@@ -7,11 +7,8 @@
 
 int main(int argc, char *argv[]) {
   
-  // static T_VBVMR_INTERFACE iVMR;
-  // load_voicemeter();
   QApplication app(argc, argv);
   MainWindow window;
-  window.loadVoicemeter();
   window.setWindowSizeLocation();
   
   window.show();
--- mainwindow.h
+++ mainwindow.h
@@ -24,10 +24,8 @@ class MainWindow : public QMainWindow
  public:
   MainWindow();
   ~MainWindow();
-  void onOutputSelected(int index);
   void setWindowSizeLocation();
   void onQuit();
-  void loadVoicemeter();
 
   void keyDown(DWORD key);
   void keyUp(DWORD key);
@@ -35,41 +33,14 @@ class MainWindow : public QMainWindow
 
 private:
   int totalItems = 3;
-  QPushButton *speakerButton;
   QPushButton *quitButton;
   QPushButton *muteButton;
-  QPushButton *cursorButton;
-  QPushButton *resetButton;
-
-  QComboBox* outputSelection;
-  QStringList outputList = {"A1", "A3"};
   QVBoxLayout *contentLayout;
 
-  QLabel *clockLabel;
-  QTimer *timer;
-  QTime myTime;
-  int villCount;
+  HHOOK hhkLowLevelKybd;
 
-  QLibrary *lib; 
+  void inputOE();
 
-  bool libraryLoaded;
-  bool mute;
-
-  bool speakerMute; 
-
-  HHOOK hhkLowLevelKybd;  
-
-
-  void logout();
-  void getMacroStatus();
-  void toggleMute();
-  void toggleSpeaker(); 
-  void centerMouseCursor();
-  void setParameterFloat(QString parameter, float pValue);
-  float getParameterFloat(QString parameter);
-
-  void onReset();
-  void onTimeout();
 };
 
 #endif // MAINWINDOW_H
